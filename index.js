a70bc2d4a0919970029cc39fa7f81a478773f65c
Darrell Ford <Darrellford85@gmail.com>
2022-03-26 04:03:47 -0700

    commit tree 91f519f0c1cd0ec5efe391313701f005d454f2a7

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+/* Test Notes to remember:
+- function called saturdayFun."This Saturday, I want to ....!"
+*/
+
+function saturdayFun(activity){
+if (activity) {
+    return `This Saturday, I want to ${activity}!`;
+    } else {
+        return `This Saturday, I want to roller-skate!`;
+    
+    }
+}
+// defines mondayWork function expression as specified//
+const mondayWork = function (monWorking) {
+    if (monWorking) {
+        return `This Monday, I will ${monWorking}.`;
+        } else {
+            return `This Monday, I will go to the office.`;
+        
+        }
+
+    };
+    /* defines wrapAdjective function according to the specification
+    - when initialized with '||' creates a function that, when called, 
+    -  wraps an adjective in a highlight: */
+
+function wrapAdjective(showEmotion = "*"){
+   const description = function (desWord = "special") {
+       return `You are ${showEmotion}${desWord}${showEmotion}!`;
+    };
+    return description;
+   
+} 
